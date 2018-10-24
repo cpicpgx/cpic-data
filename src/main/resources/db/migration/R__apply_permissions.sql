01cf6d29331647f9bf8c726af141e55c09ba34a5
Ryan Whaley <whaleyr@pharmgkb.org>
2018-10-24 10:58:04 -0700

    Adjust DB connection
    - this is now expecting a "cpic" DB role to connect with
    - grant usage does not need to be repeatable so removed (handled elsewhere)

--- src/main/resources/db/migration/R__apply_permissions.sql
+++ src/main/resources/db/migration/R__apply_permissions.sql
@@ -1,4 +1,5 @@
-grant usage on schema public to cpic_api;
+-- the role used for actual connection and CRUD
 grant select,insert,update,delete on all tables in schema public to cpic_api;
 grant usage,select on sequence cpic_id to cpic_api;
+-- the role used for read-only queries
 grant select on all tables in schema public to web_anon;
