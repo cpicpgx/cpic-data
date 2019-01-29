22a6f13807f1e81d45679b3a1fba0da76f7958a3
Ryan Whaley <whaleyr@pharmgkb.org>
2019-01-29 15:46:29 -0800

    Add test alerts exporter

---
+++ src/main/java/org/cpicpgx/exporter/TestAlertExporter.java
@@ -0,0 +1,62 @@
+package org.cpicpgx.exporter;
+
+import org.cpicpgx.db.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * An exporter for writing test alerts to an excel sheet grouped by drug.
+ *
+ * @author Ryan Whaley
+ */
+public class TestAlertExporter extends BaseExporter {
+  private static final Logger sf_logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  public static void main(String[] args) {
+    TestAlertExporter exporter = new TestAlertExporter();
+    try {
+      exporter.parseArgs(args);
+      exporter.export();
+    } catch (Exception ex) {
+      sf_logger.error("Error exporting test alerts", ex);
+    }
+  }
+  
+  private void export() throws Exception {
+    try (Connection conn = ConnectionFactory.newConnection();
+         PreparedStatement geneStmt = conn.prepareStatement("select d.name, t.drugid, max(array_length(t.trigger_condition,1)) as num_alerts from test_alerts t join drug d on t.drugid = d.drugid group by d.name, t.drugid");
+         PreparedStatement alertStmt = conn.prepareStatement("select t.trigger_condition, t.reference_point, t.cds_context, t.alert_text from test_alerts t where t.drugid=?");
+         ResultSet grs = geneStmt.executeQuery()
+    ) {
+      while (grs.next()) {
+        String drugName = grs.getString(1);
+        String drugId = grs.getString(2);
+        int triggerCount = grs.getInt(3);
+        sf_logger.info("Writing {}", drugName);
+
+        TestAlertWorkbook workbook = new TestAlertWorkbook(drugName, triggerCount);
+        alertStmt.setString(1, drugId);
+        
+        try (ResultSet ars = alertStmt.executeQuery()) {
+          int alertCount = 0;
+          while (ars.next()) {
+            workbook.writeAlert(
+                (String[])ars.getArray(1).getArray(),
+                ars.getString(2),
+                ars.getString(3),
+                (String[])ars.getArray(4).getArray());
+            alertCount++;
+          }
+          sf_logger.info("Wrote {} alerts", alertCount);
+        }
+
+        writeWorkbook(workbook);
+      }
+    }
+  }
+}
---
+++ src/main/java/org/cpicpgx/exporter/TestAlertWorkbook.java
@@ -0,0 +1,61 @@
+package org.cpicpgx.exporter;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+
+/**
+ * A workbook containing test alert language for gene phenotypes
+ *
+ * @author Ryan Whaley
+ */
+class TestAlertWorkbook extends AbstractWorkbook {
+  private static final String FILE_NAME_TEMPLATE = "%s_Pre_and_Post_Test_Alerts.xlsx";
+  
+  private String drug;
+  private Sheet sheet;
+  
+  TestAlertWorkbook(String drug, int numTriggers) {
+    super();
+    this.drug = drug;
+    
+    this.sheet = getSheet(this.drug);
+    this.colIdx = 0;
+    
+    Row headerRow = this.sheet.createRow(rowIdx++);
+    if (numTriggers == 1) {
+      writeHeaderCell(headerRow, colIdx++, "Trigger Condition");
+    } else {
+      for (int i = 1; i <= numTriggers; i++) {
+        writeHeaderCell(headerRow, colIdx++, "Trigger Condition " + i);
+      }
+    }
+    writeHeaderCell(headerRow, colIdx++, "Flow Chart Reference Point");
+    writeHeaderCell(headerRow, colIdx++, "CDS Context, Relative to Genetic Testing");
+    writeHeaderCell(headerRow, colIdx++, "CDS Alert Text");
+    
+    Integer[] columnSizes = new Integer[colIdx+1];
+    columnSizes[colIdx] = 15*256;
+    setColumnSizes(columnSizes);
+  }
+  
+  void writeAlert(String[] triggers, String flowRef, String context, String[] alertText) {
+    this.colIdx = 0;
+    Row row = this.sheet.createRow(rowIdx++);
+    for (String trigger : triggers) {
+      writeStringCell(row, colIdx++, trigger, false);
+    }
+    writeStringCell(row, colIdx++, flowRef);
+    writeStringCell(row, colIdx++, context);
+    writeStringCell(row, colIdx++, String.join("\n", alertText), wrapStyle);
+  }
+
+  @Override
+  String getFilename() {
+    return String.format(FILE_NAME_TEMPLATE, this.drug);
+  }
+
+  @Override
+  String getSheetName() {
+    return this.drug;
+  }
+}
