3077a000523d89c343c3b310be99c5e81a5d7e61
Ryan Whaley <whaleyr@pharmgkb.org>
2019-11-06 14:01:08 -0800

    Don't write out publication JSON files anymore
    - this data is coming straight from the API now

--- src/main/node/writeData.js
+++ src/main/node/writeData.js
@@ -1,11 +1,9 @@
 const alleles = require('./getAlleles');
 const guidelines = require('./getGuidelines');
 const pairs = require('./getPairs');
-const publications = require('./getPublications');
 
 const path = process.argv[2];
 
 alleles.getAlleles(path);
 guidelines.getGuidelines(path);
 pairs.getPairs(path);
-publications.getPublications(path);
