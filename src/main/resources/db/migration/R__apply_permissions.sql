60cbf2f451b3faee55593f5b1e0b61144bc3a323
Ryan Whaley <whaleyr@pharmgkb.org>
2019-11-07 10:26:46 -0800

    Add view for gene-drug pairs
    - this is a convenience for the website to simplify the API call

--- src/main/resources/db/migration/R__apply_permissions.sql
+++ src/main/resources/db/migration/R__apply_permissions.sql
@@ -3,3 +3,5 @@ grant select,insert,update,delete on all tables in schema public to cpic_api;
 grant usage,select on sequence cpic_id to cpic_api;
 -- the role used for read-only queries
 grant select on all tables in schema public to web_anon;
+
+grant select on gene_drug_pair to web_anon;
---
+++ src/main/resources/db/migration/V5__gene_drug_pair_view.sql
@@ -0,0 +1,26 @@
+CREATE VIEW gene_drug_pair AS
+select
+    d.name as drugname,
+    d.drugid,
+    p.genesymbol as gene,
+    p.level,
+    p.pgkbcalevel,
+    p.pgxtesting,
+    p.citations,
+    g.url as guidelineurl,
+    p.pairid
+from 
+     pair p 
+         join drug d on p.drugid = d.drugid 
+         left join guideline g on p.guidelineid = g.id;
+
+COMMENT ON VIEW gene_drug_pair IS 'A view of the pairs table with extra information needed for typical use';
+COMMENT ON COLUMN gene_drug_pair.drugname IS 'The name of the drug in the pair';
+COMMENT ON COLUMN gene_drug_pair.drugid IS 'The ID of the drug in the pair';
+COMMENT ON COLUMN gene_drug_pair.gene IS 'The symbol of the drug in the pair';
+COMMENT ON COLUMN gene_drug_pair.level IS 'The CPIC-assigned level of the pair';
+COMMENT ON COLUMN gene_drug_pair.pgkbcalevel IS 'The top level of PharmGKB Clinical Annotation of the pair';
+COMMENT ON COLUMN gene_drug_pair.pgxtesting IS 'The testing level of the label annotation from PharmGKB for this pair';
+COMMENT ON COLUMN gene_drug_pair.citations IS 'The PMIDs for guideline publications of this pair';
+COMMENT ON COLUMN gene_drug_pair.guidelineurl IS 'The URL for the guideline of this pair';
+COMMENT ON COLUMN gene_drug_pair.pairid IS 'The primary key ID of this pair';
