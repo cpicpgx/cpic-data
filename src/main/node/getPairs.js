c8674bc5144a1588386042c80cb0cb55c9efb177
Ryan Whaley <whaleyr@pharmgkb.org>
2019-11-07 20:38:47 -0800

    Update pairs data retrieval script
    - simplify the API call using the new view
    - remove unused mapping and sorting
    - remove count by status, no longer used

--- src/main/node/getPairs.js
+++ src/main/node/getPairs.js
@@ -8,33 +8,19 @@ const defaultFilename = 'cpicPairs';
 
 exports.getPairs = (path) => {
   axios.get(
-    cpicapi.apiUrl('/pair'),
-    {params: {select: '*,drug(name),guideline(url)', order: 'level,genesymbol'}})
+    cpicapi.apiUrl('/gene_drug_pair'),
+    {params: {order: 'level,drugname'}})
     .then((r) => {
-      const pairs = _.sortBy(r.data.map(p => {
-        return {
-          chemical: {name: p.drug.name},
-          chemicalName: p.drug.name,
-          cpicLevel: p.level,
-          gene: {name: p.genesymbol},
-          guidelineId: p.pairid,
-          label: {id: 'x', name: p.pgxtesting},
-          pgkbLevel: p.pgkbcalevel,
-          pmids: p.citations,
-          url: _.get(p, 'guideline.url'),
-        }
-      }), ['cpicLevel', 'chemicalName']);
-      const countByStatus = _.countBy(pairs.map(p => p.cpicLevel));
-      const countDrugs = _.uniq(pairs.map(p => _.get(p, 'chemical.name'))).length;
-      const countGenes = _.uniq(pairs.map(p => _.get(p, 'gene.name'))).length;
+      const pairs = r.data;
+      const countDrugs = _.uniq(pairs.map(p => _.get(p, 'drugname'))).length;
+      const countGenes = _.uniq(pairs.map(p => _.get(p, 'gene'))).length;
       const lastUpdated = dayjs().format('MMM D, YYYY');
       const payload = {
-        countByStatus: countByStatus,
-        countDrugs: countDrugs,
-        countGenes: countGenes,
+        countDrugs,
+        countGenes,
         countGuidelines: pairs.length,
-        lastUpdated: lastUpdated,
-        pairs: pairs,
+        lastUpdated,
+        pairs,
       };
 
       const jsonFile = `${path}/${defaultFilename}.json`;
@@ -46,7 +32,7 @@ exports.getPairs = (path) => {
       const csvFile = `${path}/${defaultFilename}.csv`;
       const csv = _.concat(
         [`"Date last updated: ${lastUpdated}"`, 'Gene,Drug,Guideline,CPIC Level,PharmGKB Level of Evidence,PGx on FDA Label,CPIC Publications (PMID)'],
-        pairs.map((p) => [p.gene.name, p.chemical.name, p.url, p.cpicLevel, p.pgkbLevel, p.label.name, _.join(p.pmids, ';')].join(',')),
+        pairs.map((p) => [p.gene, p.drugname, p.guidelineurl, p.level, p.pgkbcalevel, p.pgxtesting, _.join(p.citations, ';')].join(',')),
       );
       fs.writeFile(csvFile, csv.join('\n'), (e) => {
         if (e) console.log(e);
