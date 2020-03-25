f8c16b422edf2f5726ac3c9623fd56ed37840ba9
Ryan Whaley <whaleyr@pharmgkb.org>
2020-03-25 15:58:47 -0700

    Update pair ca level and pgx testing from PharmGKB

---
+++ src/main/resources/db/migration/V7__nsaid_pair_data.sql
@@ -0,0 +1,17 @@
+update pair set pgkbcalevel='3' where pairid=110001;
+update pair set pgkbcalevel='3' where pairid=110011;
+update pair set pgkbcalevel='3' where pairid=110010;
+update pair set pgkbcalevel='3' where pairid=110008;
+update pair set pgkbcalevel='3' where pairid=110004;
+update pair set pgkbcalevel='1A' where pairid=110064;
+update pair set pgkbcalevel='2A' where pairid=136681;
+update pair set pgkbcalevel='1A' where pairid=110090;
+update pair set pgkbcalevel='1A' where pairid=110097;
+update pair set pgkbcalevel='1A' where pairid=136683;
+update pair set pgkbcalevel='1A' where pairid=136685;
+update pair set pgkbcalevel='1A' where pairid=136687;
+update pair set pgxTesting='Actionable PGx' where pairid=136687;
+update pair set pgkbcalevel='1A' where pairid=136691;
+update pair set pgxTesting='Actionable PGx' where pairid=136691;
+update pair set pgkbcalevel='1A' where pairid=110120;
+update pair set pgkbcalevel='3' where pairid=110301;
