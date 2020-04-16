c04ddc4b28efa55af85afc1c42c8e43f0b8d221c
Ryan Whaley <whaleyr@pharmgkb.org>
2020-04-16 10:01:53 -0700

    Add "reload" option to FlywayMigrate

--- src/main/java/org/cpicpgx/db/FlywayMigrate.java
+++ src/main/java/org/cpicpgx/db/FlywayMigrate.java
@@ -1,5 +1,6 @@
 package org.cpicpgx.db;
 
+import org.apache.commons.cli.*;
 import org.flywaydb.core.Flyway;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,17 +14,34 @@ public class FlywayMigrate {
   private static final Logger sf_logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
   public static void main(String[] args) {
-    Flyway flyway = Flyway.configure().dataSource(
-        ConnectionFactory.getJdbcUrl(),
-        ConnectionFactory.getUser(),
-        ConnectionFactory.getPass()
-    ).load();
-    if (flyway.info().current() == null) {
-      sf_logger.info("Starting fresh");
-    } else {
-      sf_logger.info("Starting at version {}", flyway.info().current().getVersion());
+    Options options = new Options();
+    options.addOption("r", false, "flag to reset (clean) the DB before migration");
+    CommandLineParser clParser = new DefaultParser();
+
+    try {
+      CommandLine cli = clParser.parse(options, args);
+
+      Flyway flyway = Flyway.configure().dataSource(
+          ConnectionFactory.getJdbcUrl(),
+          ConnectionFactory.getUser(),
+          ConnectionFactory.getPass()
+      ).load();
+
+      if (cli.hasOption("r")) {
+        sf_logger.info("Cleaning DB, as requested");
+        flyway.clean();
+      }
+
+      if (flyway.info().current() == null) {
+        sf_logger.info("Starting fresh");
+      } else {
+        sf_logger.info("Starting at version {}", flyway.info().current().getVersion());
+      }
+      int migrations = flyway.migrate();
+      sf_logger.info("Applied {} changes, now at version {}", migrations, flyway.info().current().getVersion());
     }
-    int migrations = flyway.migrate();
-    sf_logger.info("Applied {} changes, now at version {}", migrations, flyway.info().current().getVersion());
+    catch (ParseException ex) {
+      sf_logger.error("Error parsing args", ex);
+    }
   }
 }
