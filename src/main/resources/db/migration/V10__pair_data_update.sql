c694f71d0727d303c459a7fa85331326aa68fe63
Ryan Whaley <whaleyr@pharmgkb.org>
2020-04-30 12:15:37 -0700

    Update pair data from PharmGKB

---
+++ src/main/resources/db/migration/V10__pair_data_update.sql
@@ -0,0 +1,7 @@
+update pair set pgkbcalevel='2A' where genesymbol='CYP2C8' and drugid='RxNorm:5640';
+update pair set pgkbcalevel='1A' where genesymbol='CYP2C9' and drugid='RxNorm:5640';
+update pair set pgkbcalevel='1A' where genesymbol='CYP2C9' and drugid='RxNorm:20890';
+update pair set pgkbcalevel='1A' where genesymbol='CYP2C9' and drugid='RxNorm:41493';
+update pair set pgxtesting='Actionable PGx' where genesymbol='CYP2C9' and drugid='RxNorm:41493';
+update pair set pgkbcalevel='1A' where genesymbol='CYP2C9' and drugid='RxNorm:8356';
+update pair set pgxtesting='Actionable PGx' where genesymbol='CYP2C9' and drugid='RxNorm:8356';
