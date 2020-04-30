2bc37852b0555d04cbd0fc9ce66ff6ac3d50c634
Ryan Whaley <whaleyr@pharmgkb.org>
2020-04-29 20:13:21 -0700

    Add DrugResourceExporter
    - export drug reference ID files

---
+++ src/main/java/org/cpicpgx/exporter/DrugResourceExporter.java
@@ -0,0 +1,64 @@
+package org.cpicpgx.exporter;
+
+import org.cpicpgx.db.ConnectionFactory;
+import org.cpicpgx.model.EntityType;
+import org.cpicpgx.model.FileType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * A class to export files, by drug, of the external IDs used to identify that drug
+ *
+ * @author Ryan Whaley
+ */
+public class DrugResourceExporter extends BaseExporter {
+  private static final Logger sf_logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  public static void main(String[] args) {
+    DrugResourceExporter exporter = new DrugResourceExporter();
+    try {
+      exporter.parseArgs(args);
+      exporter.export();
+    } catch (Exception ex) {
+      sf_logger.error("Error exporting drug resource", ex);
+    }
+  }
+
+  @Override
+  FileType getFileType() {
+    return FileType.DRUG_RESOURCE;
+  }
+
+  @Override
+  EntityType getEntityCategory() {
+    return EntityType.DRUG;
+  }
+
+  @Override
+  public void export() throws Exception {
+    try (
+        Connection conn = ConnectionFactory.newConnection();
+        PreparedStatement drugStmt = conn.prepareStatement("select drugid, name, pharmgkbid, rxnormid, drugbankid, atcid, umlscui from drug d");
+        ResultSet rs = drugStmt.executeQuery()
+        ) {
+      while (rs.next()) {
+        DrugResourceWorkbook workbook = new DrugResourceWorkbook(rs.getString(2));
+        workbook.writeMapping(
+            rs.getString(4),
+            rs.getString(5),
+            (String[])rs.getArray(6).getArray(),
+            rs.getString(3)
+        );
+
+        writeWorkbook(workbook);
+        addFileExportHistory(workbook.getFilename(), new String[]{rs.getString(1)});
+      }
+      handleFileUpload();
+    }
+  }
+}
---
+++ src/main/java/org/cpicpgx/exporter/DrugResourceWorkbook.java
@@ -0,0 +1,51 @@
+package org.cpicpgx.exporter;
+
+import com.google.common.base.Joiner;
+import org.apache.poi.ss.usermodel.Row;
+
+/**
+ * A workbook to list the IDs for a drug in external resources (e.g. ATC, RxNorm)
+ *
+ * @author Ryan Whaley
+ */
+public class DrugResourceWorkbook extends AbstractWorkbook {
+  private static final String NAME_TEMPLATE = "%s-Drug_Resource_Mappings.xlsx";
+  private static final String SHEET_NAME = "mapping";
+  private final SheetWrapper sheet;
+  private final String drug;
+
+  DrugResourceWorkbook(String drugName) {
+    super();
+    this.drug = drugName;
+
+    this.sheet = findSheet(SHEET_NAME);
+
+    Row row = this.sheet.nextRow();
+    writeHeaderCell(row, 0, "Drug or Ingredient");
+    writeHeaderCell(row, 1, "Source");
+    writeHeaderCell(row, 2, "Code Type");
+    writeHeaderCell(row, 3, "Code");
+
+    this.sheet.setColCount(4);
+  }
+
+  void writeMapping(String rxnorm, String drugbank, String[] atc, String pharmgkb) {
+    writeRow("RxNorm", "RxCUI", rxnorm);
+    writeRow("DrugBank", "Accession Number", drugbank);
+    writeRow("ATC", "ATC Code", Joiner.on(";").join(atc));
+    writeRow("PharmGKB", "PharmGKB Accession ID", pharmgkb);
+  }
+
+  void writeRow(String system, String idType, String idValue) {
+    Row row = this.sheet.nextRow();
+    writeStringCell(row, 0, this.drug, false);
+    writeStringCell(row, 1, system, false);
+    writeStringCell(row, 2, idType, false);
+    writeStringCell(row, 3, idValue, false);
+  }
+
+  @Override
+  String getFilename() {
+    return String.format(NAME_TEMPLATE, this.drug.replaceAll("/", "_"));
+  }
+}
