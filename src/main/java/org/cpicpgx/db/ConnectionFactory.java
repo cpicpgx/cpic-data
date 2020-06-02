e61d5302b10d964d5b0dd7cddbea0f491185f0fa
Ryan Whaley <whaleyr@pharmgkb.org>
2020-06-02 14:35:13 -0700

    feat: Make db configurable via envvar

--- src/main/java/org/cpicpgx/db/ConnectionFactory.java
+++ src/main/java/org/cpicpgx/db/ConnectionFactory.java
@@ -12,10 +12,11 @@ import java.sql.SQLException;
  * @author Ryan Whaley
  */
 public class ConnectionFactory {
-  private static final String sf_dbUrl = "jdbc:postgresql://%s/cpic";
-  private static final String sf_host = MoreObjects.firstNonNull(System.getenv("CPIC_DB"), "localhost");
+  private static final String sf_dbUrl = "jdbc:postgresql://%s/%s";
+  private static final String sf_host = MoreObjects.firstNonNull(System.getenv("CPIC_HOST"), "localhost");
   private static final String sf_user = MoreObjects.firstNonNull(System.getenv("CPIC_USER"), "cpic");
   private static final String sf_pass = MoreObjects.firstNonNull(System.getenv("CPIC_PASS"), "");
+  private static final String sf_db = MoreObjects.firstNonNull(System.getenv("CPIC_DB"), "cpic");
 
   /**
    * Makes a new {@link Connection}. All connection information defaults to local DB but also can override with environment variables.
@@ -23,11 +24,11 @@ public class ConnectionFactory {
    * @throws SQLException can occur if there is a problem connecting to the database
    */
   public static Connection newConnection() throws SQLException {
-    return DriverManager.getConnection(String.format(getJdbcUrl(), sf_host), sf_user, sf_pass);
+    return DriverManager.getConnection(getJdbcUrl(), sf_user, sf_pass);
   }
   
   static String getJdbcUrl() {
-    return String.format(sf_dbUrl, sf_host);
+    return String.format(sf_dbUrl, sf_host, sf_db);
   }
 
   static String getUser() {
