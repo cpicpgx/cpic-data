9b5e06d54f4fb0a27234cac97b6553bb8f785e02
Ryan Whaley <whaleyr@pharmgkb.org>
2020-07-16 16:21:57 -0700

    feat: update test alert parsing for history and populations
    - parse an optional "Change log" tab if present
    - only store unique "Notes" for the whole sheet, not per-population
    - population names may or may not have the word "population" in them so make it optional

--- src/main/java/org/cpicpgx/importer/TestAlertImporter.java
+++ src/main/java/org/cpicpgx/importer/TestAlertImporter.java
@@ -7,7 +7,7 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.cpicpgx.db.ConnectionFactory;
 import org.cpicpgx.db.DbLookup;
 import org.cpicpgx.db.NoteType;
-import org.cpicpgx.exception.NotFoundException;
+import org.cpicpgx.exporter.AbstractWorkbook;
 import org.cpicpgx.model.FileType;
 import org.cpicpgx.util.RowWrapper;
 import org.cpicpgx.util.WorkbookWrapper;
@@ -18,6 +18,7 @@ import java.lang.invoke.MethodHandles;
 import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -39,6 +40,8 @@ public class TestAlertImporter extends BaseDirectoryImporter {
 
   private static final String FILE_EXTENSION = "_Pre_and_Post_Test_Alerts.xlsx";
   private static final String DEFAULT_DIRECTORY = "test_alerts";
+
+  private final Set<String> f_notes = new LinkedHashSet<>();
 
   public static void main(String[] args) {
     rebuild(new TestAlertImporter(), args);
@@ -67,16 +70,23 @@ public class TestAlertImporter extends BaseDirectoryImporter {
 
   @Override
   void processWorkbook(WorkbookWrapper workbook) throws Exception {
-    for (Iterator<Sheet> sheetIterator = workbook.getSheetIterator(); sheetIterator.hasNext();) {
-      Sheet sheet = sheetIterator.next();
-      if (!sheet.getSheetName().startsWith("population ")) {
-        throw new RuntimeException("Sheet name does not start with \"population\": " + sheet.getSheetName());
-      }
+    try (DbHarness dbHarness = new DbHarness()) {
+      for (Iterator<Sheet> sheetIterator = workbook.getSheetIterator(); sheetIterator.hasNext(); ) {
+        Sheet sheet = sheetIterator.next();
 
-      String population = sheet.getSheetName().replaceFirst("population ", "");
-      try (Connection conn = ConnectionFactory.newConnection()) {
-        processTwoTrigger(workbook, conn, population);
+        if (AbstractWorkbook.HISTORY_SHEET_NAME.equalsIgnoreCase(sheet.getSheetName())) {
+          workbook.currentSheetIs(AbstractWorkbook.HISTORY_SHEET_NAME);
+          processHistory(workbook, dbHarness);
+        }
+        else if (!sheet.getSheetName().equalsIgnoreCase("flow chart")) {
+          String population = sheet.getSheetName().replaceFirst("population ", "");
+          processTestAlertSheet(workbook, dbHarness, population);
+        }
       }
+      for (String note : f_notes) {
+        dbHarness.writeNote(note);
+      }
+      f_notes.clear();
     }
   }
 
@@ -85,13 +95,7 @@ public class TestAlertImporter extends BaseDirectoryImporter {
   private static final String COL_NAME_CONTEXT = "CDS Context, Relative to Genetic Testing";
   private static final String COL_NAME_ALERT = "CDS Alert Text";
 
-  private void processTwoTrigger(WorkbookWrapper workbook, Connection conn, String population) throws Exception {
-    PreparedStatement insert = conn.prepareStatement(
-        "insert into test_alert(cds_context, genes, drugid, alert_text, population, activity_score, phenotype) values (?, ?, ?, ?, ?, ?::jsonb, ?::jsonb)");
-    PreparedStatement insertNote = conn.prepareStatement(
-        "insert into drug_note(drugId, type, ordinal, note) values (?, ?, ?, ?)");
-    DrugCache drugCache = new DrugCache(conn);
-
+  private void processTestAlertSheet(WorkbookWrapper workbook, DbHarness dbHarness, String population) throws Exception {
     RowWrapper headerRow = workbook.getRow(0);
 
     Map<String, Integer> activityCols = new TreeMap<>();
@@ -124,7 +128,6 @@ public class TestAlertImporter extends BaseDirectoryImporter {
       }
     }
 
-    List<String> notes = new ArrayList<>();
     boolean noteMode = false;
     for (int i = 1; i <= workbook.currentSheet.getLastRowNum(); i++) {
       sf_logger.debug("reading row {}", i);
@@ -141,8 +144,8 @@ public class TestAlertImporter extends BaseDirectoryImporter {
 
       // if there's only text in the first column, it must be a note
       if (noteMode) {
-        if (row.getNullableText(0) != null) {
-          notes.add(row.getNullableText(0));
+        if (!row.hasNoText(0)) {
+          f_notes.add(row.getText(0));
         }
         continue;
       }
@@ -158,44 +161,93 @@ public class TestAlertImporter extends BaseDirectoryImporter {
         phenotypeJson.addProperty(gene, phenotype);
       }
 
-      String drugId = drugCache.lookup(row.getNullableText(COL_DRUG));
-      Array geneArray = conn.createArrayOf("VARCHAR", activityCols.keySet().toArray());
       String context = StringUtils.replace(row.getNullableText(idxContext), "Test", "test");
-      Array alertSqlArray = conn.createArrayOf("VARCHAR", new String[]{row.getNullableText(idxAlert)});
+
+      dbHarness.writeAlert(context, activityCols.keySet(), row.getText(COL_DRUG), row.getNullableText(idxAlert), population, activityJson, phenotypeJson);
+    }
+  }
+
+  void processHistory(WorkbookWrapper workbook, DbHarness dbHarness) throws SQLException {
+    for (int i = 1; i <= workbook.currentSheet.getLastRowNum(); i++) {
+      RowWrapper row = workbook.getRow(i);
+      if (row.hasNoText(0) ^ row.hasNoText(1)) {
+        throw new RuntimeException("Change log row " + (i + 1) + ": row must have both date and text");
+      } else if (row.hasNoText(0)) continue;
+
+      java.util.Date date = row.getDate(0);
+      String note = row.getText(1);
+      dbHarness.writeHistory(date, note, i - 1);
+    }
+  }
+
+  private class DbHarness implements AutoCloseable {
+    private final Connection conn;
+    private final PreparedStatement insert;
+    private final PreparedStatement insertNote;
+    private final PreparedStatement insertChangeStmt;
+    private final Map<String, String> nameToIdMap = new HashMap<>();
+    private final List<AutoCloseable> closables = new ArrayList<>();
+
+    private DbHarness() throws SQLException {
+      this.conn = ConnectionFactory.newConnection();
+      this.insert = conn.prepareStatement(
+          "insert into test_alert(cds_context, genes, drugid, alert_text, population, activity_score, phenotype) values (?, ?, ?, ?, ?, ?::jsonb, ?::jsonb)");
+      this.insertNote = conn.prepareStatement(
+          "insert into drug_note(drugId, type, ordinal, note) values (?, ?, ?, ?)");
+      this.insertChangeStmt = conn.prepareStatement(
+          "insert into drug_note(drugid, note, type, ordinal, date) values (?, ?, ?, ?, ?)");
+
+      closables.add(this.insert);
+      closables.add(this.insertNote);
+      closables.add(this.insertChangeStmt);
+      closables.add(conn);
+    }
+
+    int noteOrdinal = 0;
+    private void writeNote(String note) throws SQLException {
+      for (String drugId : nameToIdMap.values()) {
+        insertNote.clearParameters();
+        insertNote.setString(1, drugId);
+        insertNote.setString(2, getNoteType().name());
+        insertNote.setInt(3, noteOrdinal);
+        insertNote.setString(4, note);
+        insertNote.executeUpdate();
+      }
+      noteOrdinal += 1;
+    }
+
+    private void writeHistory(Date date, String note, int ordinal) throws SQLException {
+      for (String drugId : nameToIdMap.values()) {
+        this.insertChangeStmt.clearParameters();
+        this.insertChangeStmt.setString(1, drugId);
+        if (StringUtils.isNotBlank(note)) {
+          this.insertChangeStmt.setString(2, note);
+        } else {
+          this.insertChangeStmt.setString(2, "n/a");
+        }
+        this.insertChangeStmt.setString(3, getNoteType().name());
+        this.insertChangeStmt.setInt(4, ordinal);
+        this.insertChangeStmt.setDate(5, new java.sql.Date(date.getTime()));
+        this.insertChangeStmt.executeUpdate();
+      }
+    }
+
+    private void writeAlert(String context, Set<String> genes, String drugName, String alertText, String population, JsonObject activityJson, JsonObject phenotypeJson) throws SQLException {
+      Array geneArray = conn.createArrayOf("VARCHAR", genes.toArray());
+      Array alertSqlArray = conn.createArrayOf("VARCHAR", new String[]{alertText});
 
       insert.clearParameters();
       insert.setString(1, context);
       insert.setArray(2, geneArray);
-      insert.setString(3, drugId);
+      insert.setString(3, lookup(drugName));
       insert.setArray(4, alertSqlArray);
       insert.setString(5, population);
       insert.setString(6, activityJson.toString());
-      //noinspection JpaQueryApiInspection
       insert.setString(7, phenotypeJson.toString());
       insert.executeUpdate();
     }
 
-    for (int i = 0; i < notes.size(); i++) {
-      String note = notes.get(i);
-      for (String drugId : drugCache.getIds()) {
-        insertNote.setString(1, drugId);
-        insertNote.setString(2, NoteType.TEST_ALERT.name());
-        insertNote.setInt(3, i);
-        insertNote.setString(4, note);
-        insertNote.executeUpdate();
-      }
-    }
-  }
-
-  private static class DrugCache {
-    private final Connection conn;
-    private final Map<String, String> nameToIdMap = new HashMap<>();
-
-    private DrugCache(Connection conn) {
-      this.conn = conn;
-    }
-
-    private String lookup(String name) throws Exception {
+    private String lookup(String name) throws SQLException {
       if (StringUtils.isBlank(name)) {
         return null;
       }
@@ -203,14 +255,17 @@ public class TestAlertImporter extends BaseDirectoryImporter {
       String id = nameToIdMap.get(name);
       if (id == null) {
         id = DbLookup.getDrugByName(conn, name)
-                .orElseThrow(() -> new NotFoundException("No drug for " + name));
+                .orElseThrow(() -> new RuntimeException("No drug for " + name));
         nameToIdMap.put(name, id);
       }
       return id;
     }
 
-    private Collection<String> getIds() {
-      return this.nameToIdMap.values();
+    @Override
+    public void close() throws Exception {
+      for (AutoCloseable closable : closables) {
+        closable.close();
+      }
     }
   }
 }
