0253b116999e625c029a82a35a2fcd23111d66b9
Ryan Whaley <whaleyr@pharmgkb.org>
2020-07-28 15:44:29 -0700

    chore: add another highlighted publication

---
+++ src/main/resources/db/migration/V19__new_publication.sql
@@ -0,0 +1,3 @@
+COPY publication (title, authors, journal, month, page, volume, year, pmid, highlightedonsite) FROM stdin;
+The Clinical Pharmacogenetics Implementation Consortium: 10 Years Later.	{"Relling Mary V","Klein Teri E","Gammal Roseann S","Whirl-Carrillo Michelle","Hoffman James M","Caudle Kelly E"}	Clinical pharmacology and therapeutics	1			2020	31562822	true
+\.
