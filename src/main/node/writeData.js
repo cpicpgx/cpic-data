536b625f92488bd71111521656539d5f4525421f
Ryan Whaley <whaleyr@pharmgkb.org>
2020-08-12 15:40:36 -0700

    feat: add back getPublications.js to cache pub data

--- src/main/node/writeData.js
+++ src/main/node/writeData.js
@@ -1,9 +1,11 @@
 const alleles = require('./getAlleles');
 const guidelines = require('./getGuidelines');
 const pairs = require('./getPairs');
+const pub = require('./getPublications');
 
 const path = process.argv[2];
 
 alleles.getAlleles(path);
 guidelines.getGuidelines(path);
 pairs.getPairs(path);
+pub.getPublications(path);
