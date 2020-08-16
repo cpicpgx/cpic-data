20c5075e268f81c34134be2a2a4b1c6ce4c972f3
Ryan Whaley <whaleyr@pharmgkb.org>
2020-08-16 11:16:37 -0700

    chore: clear out server files

--- server/api.roles.sql
+++
@@ -1,8 +0,0 @@
--- cpic_api is the role that postgrest connects with
-create user cpic_api with password 'cpicapi';
-grant usage on schema cpic to cpic_api;
-grant usage on schema cpic to web_anon;
-
--- the web_anon role is the set of permissions for anonymous read usage of the data
-create role web_anon;
-grant web_anon to cpic_api;
