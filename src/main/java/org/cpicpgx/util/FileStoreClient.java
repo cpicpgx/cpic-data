bb34ab238fe1f45fc3b2069e5c4baf7bc21cb20e
Ryan Whaley <whaleyr@pharmgkb.org>
2020-08-24 14:20:45 -0700

    feat: write reports to dated S3 paths and a "current" path

--- src/main/java/org/cpicpgx/util/FileStoreClient.java
+++ src/main/java/org/cpicpgx/util/FileStoreClient.java
@@ -11,6 +11,8 @@ import java.io.File;
 import java.lang.invoke.MethodHandles;
 import java.nio.file.Path;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Utility class that helps write files to the network file store, Amazon S3.
@@ -27,6 +29,7 @@ public class FileStoreClient implements AutoCloseable {
   private static final String S3_PUBLIC_BUCKET      = "files.cpicpgx.org";
   private static final String S3_GENERIC_KEY_PREFIX = "data/report/";
   private static final String S3_URL_FORMAT         = "http://" + S3_PUBLIC_BUCKET + "/%s%s";
+  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
   
   private final AmazonS3 s3;
   private FileHistoryWriter fileHistoryWriter;
@@ -42,18 +45,23 @@ public class FileStoreClient implements AutoCloseable {
   
   public void putArtifact(Path geneFilePath, FileType type) {
     String fileName = geneFilePath.getFileName().toString();
-    String directoryPrefix = S3_GENERIC_KEY_PREFIX + type.name().toLowerCase() + "/";
-    putFile(directoryPrefix, fileName, geneFilePath.toFile());
+
+    String datedDirPath = S3_GENERIC_KEY_PREFIX + DATE_FORMAT.format(new Date()) + "/" + type.name().toLowerCase() + "/";
+    putFile(datedDirPath, fileName, geneFilePath.toFile());
+
+    String currentDirPath = S3_GENERIC_KEY_PREFIX + "current/" + type.name().toLowerCase() + "/";
+    putFile(currentDirPath, fileName, geneFilePath.toFile());
+
     try {
-      fileHistoryWriter.writeUpload(fileName, String.format(S3_URL_FORMAT, directoryPrefix, fileName));
+      fileHistoryWriter.writeUpload(fileName, String.format(S3_URL_FORMAT, datedDirPath, fileName));
     } catch (SQLException e) {
       sf_logger.error("Error updating file record in DB " + fileName, e);
     }
   }
 
-  private void putFile(String prefix, String fileName, File geneFile) {
-    s3.putObject(S3_PUBLIC_BUCKET, prefix + fileName, geneFile);
-    sf_logger.info("Uploaded {}", String.format("s3:///%s/%s%s", S3_PUBLIC_BUCKET, prefix, fileName));
+  private void putFile(String directoryPath, String fileName, File geneFile) {
+    s3.putObject(S3_PUBLIC_BUCKET, directoryPath + fileName, geneFile);
+    sf_logger.info("Uploaded {}", String.format("s3:///%s/%s%s", S3_PUBLIC_BUCKET, directoryPath, fileName));
   }
   
   @Override
