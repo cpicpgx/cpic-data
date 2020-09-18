bf91442dd707353304efd4e0904fe39e77ad7901
Ryan Whaley <whaleyr@pharmgkb.org>
2020-09-18 10:38:52 -0700

    chore: change output path for node data script

--- src/main/node/writeData.js
+++ src/main/node/writeData.js
@@ -1,11 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const alleles = require('./getAlleles');
 const guidelines = require('./getGuidelines');
 const pairs = require('./getPairs');
 const pub = require('./getPublications');
 
-const path = process.argv[2];
+const outDirectoryPath = path.join(__dirname, '..', '..', '..', 'out');
+if (!fs.existsSync(outDirectoryPath)) {
+  fs.mkdirSync(outDirectoryPath);
+}
 
-alleles.getAlleles(path);
-guidelines.getGuidelines(path);
-pairs.getPairs(path);
-pub.getPublications(path);
+alleles.getAlleles(outDirectoryPath);
+guidelines.getGuidelines(outDirectoryPath);
+pairs.getPairs(outDirectoryPath);
+pub.getPublications(outDirectoryPath);
