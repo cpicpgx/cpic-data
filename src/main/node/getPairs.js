8cacdec73d08a945804db969b34738bf437d8a9f
Ryan Whaley <whaleyr@pharmgkb.org>
2020-10-02 10:27:10 -0700

    fix: add CPIC Level Status to pairs download CSV
    - also add papaparse to properly format CSV data

--- src/main/node/getPairs.js
+++ src/main/node/getPairs.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const cpicapi = require('./cpicapi');
 const dayjs = require('dayjs');
 const fs = require('fs');
+const Papa = require('papaparse');
 const _ = require('lodash');
 
 const defaultFilename = 'cpicPairs';
@@ -11,6 +12,8 @@ exports.getPairs = (path) => {
     cpicapi.apiUrl('/pair_view'),
     {params: {order: 'cpiclevel,drugname'}})
     .then((r) => {
+
+      // Compile the data from the API response
       const pairs = r.data;
       const countDrugs = _.uniq(pairs.map(p => _.get(p, 'drugname'))).length;
       const countGenes = _.uniq(pairs.map(p => _.get(p, 'genesymbol'))).length;
@@ -29,18 +32,38 @@ exports.getPairs = (path) => {
         }),
       };
 
+      // Make the JSON file
       const jsonFile = `${path}/${defaultFilename}.json`;
       fs.writeFile(jsonFile, JSON.stringify(payload, null, 2), (e) => {
         if (e) console.log(e);
         console.log(`Done writing ${jsonFile}`);
       });
 
+      // Make the CSV file
+      const fields = [
+        'Gene',
+        'Drug',
+        'Guideline',
+        'CPIC Level',
+        'CPIC Level Status',
+        'PharmGKB Level of Evidence',
+        'PGx on FDA Label',
+        'CPIC Publications (PMID)'
+      ];
+      const data = pairs.map((p) => [
+        p.genesymbol,
+        p.drugname,
+        p.guidelineurl,
+        p.cpiclevel,
+        p.provisional ? 'Provisional' : 'Final',
+        p.pgkbcalevel,
+        p.pgxtesting,
+        _.join(p.pmids, ';')
+      ]);
+      const csv = Papa.unparse({fields, data});
+
       const csvFile = `${path}/${defaultFilename}.csv`;
-      const csv = _.concat(
-        [`"Date last updated: ${lastUpdated}"`, 'Gene,Drug,Guideline,CPIC Level,PharmGKB Level of Evidence,PGx on FDA Label,CPIC Publications (PMID)'],
-        pairs.map((p) => [p.genesymbol, `"${p.drugname}"`, p.guidelineurl, p.cpiclevel, p.pgkbcalevel, p.pgxtesting, _.join(p.pmids, ';')].join(',')),
-      );
-      fs.writeFile(csvFile, csv.join('\n'), (e) => {
+      fs.writeFile(csvFile, `"Date last updated: ${lastUpdated}"\n` + csv, (e) => {
         if (e) console.log(e);
         console.log(`Done writing ${csvFile}`);
       });
