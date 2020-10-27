7993c3782e1ec67c6470dab5ef6ecbae1e30d684
Ryan Whaley <whaleyr@pharmgkb.org>
2020-10-27 12:48:31 -0700

    fix: limit AlleleFunctionalityReferenceExporter export and sort properly
    - sort the alleles in alphanumeric order
    - limit to only alleles with a clinical function assign

--- src/main/java/org/cpicpgx/exporter/AlleleFunctionalityReferenceExporter.java
+++ src/main/java/org/cpicpgx/exporter/AlleleFunctionalityReferenceExporter.java
@@ -4,12 +4,15 @@ import org.cpicpgx.db.ConnectionFactory;
 import org.cpicpgx.model.FileType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se.sawano.java.text.AlphanumericComparator;
 
 import java.lang.invoke.MethodHandles;
 import java.sql.Array;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
  * This class queries the functional_reference table and then dumps the contents out to excel workbooks
@@ -35,33 +38,48 @@ public class AlleleFunctionalityReferenceExporter extends BaseExporter {
 
   public void export() throws Exception {
     try (Connection conn = ConnectionFactory.newConnection();
-         PreparedStatement geneStmt = conn.prepareStatement("select distinct a.genesymbol from allele a where clinicalfunctionalstatus is not null order by 1");
-         PreparedStatement alleleStmt = conn.prepareStatement("select a.name, a.activityvalue, a.functionalstatus, a.clinicalfunctionalstatus, a.clinicalfunctionalsubstrate, " +
-             "a.citations, a.strength, a.findings, a.functioncomments " +
-             "from allele a where a.genesymbol=? order by a.id");
+         PreparedStatement geneStmt = conn.prepareStatement(
+             "select distinct a.genesymbol from allele a where clinicalfunctionalstatus is not null order by 1");
+         PreparedStatement alleleListStmt = conn.prepareStatement(
+             "select name from allele where genesymbol=? and clinicalfunctionalstatus is not null");
+         PreparedStatement alleleStmt = conn.prepareStatement(
+             "select a.name, a.activityvalue, a.functionalstatus, a.clinicalfunctionalstatus, " +
+                 "a.clinicalfunctionalsubstrate, a.citations, a.strength, a.findings, a.functioncomments " +
+                 "from allele a where a.genesymbol=? and a.name=?");
          ResultSet grs = geneStmt.executeQuery()
     ) {
       while (grs.next()) {
         String symbol = grs.getString(1);
         
         AlleleFunctionalityReferenceWorkbook workbook = new AlleleFunctionalityReferenceWorkbook(symbol);
-        
-        alleleStmt.setString(1, symbol);
-        try (ResultSet rs = alleleStmt.executeQuery()) {
+        alleleListStmt.setString(1, symbol);
+        SortedSet<String> alleleNames = new TreeSet<>(new AlphanumericComparator());
+        try (ResultSet rs = alleleListStmt.executeQuery()) {
           while (rs.next()) {
-            String alleleName = rs.getString(1);
-            String activity = rs.getString(2);
-            String function = rs.getString(3);
-            String clinFunction = rs.getString(4);
-            String clinSubstrate = rs.getString(5);
-            Array ctiations = rs.getArray(6);
-            String strength = rs.getString(7);
-            String findings = rs.getString(8);
-            String comments = rs.getString(9);
+            alleleNames.add(rs.getString(1));
+          }
+        }
 
-            String[] citationArray = ctiations == null ? new String[]{} : (String[])ctiations.getArray();
+        for (String alleleName : alleleNames) {
+          alleleStmt.setString(1, symbol);
+          alleleStmt.setString(2, alleleName);
+          try (ResultSet rs = alleleStmt.executeQuery()) {
+            if (rs.next()) {
+              String activity = rs.getString(2);
+              String function = rs.getString(3);
+              String clinFunction = rs.getString(4);
+              String clinSubstrate = rs.getString(5);
+              Array citations = rs.getArray(6);
+              String strength = rs.getString(7);
+              String findings = rs.getString(8);
+              String comments = rs.getString(9);
 
-            workbook.writeAlleleRow(alleleName, activity, function, clinFunction, clinSubstrate, citationArray, strength, findings, comments);
+              String[] citationArray = citations == null ? new String[]{} : (String[]) citations.getArray();
+
+              workbook.writeAlleleRow(alleleName, activity, function, clinFunction, clinSubstrate, citationArray, strength, findings, comments);
+            } else {
+              throw new RuntimeException("Allele name not found: " + alleleName);
+            }
           }
         }
         
