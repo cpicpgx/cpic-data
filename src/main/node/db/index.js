038f27ffeff01a17e26912eb446cad18c2df4eb3
Ryan Whaley <whaleyr@pharmgkb.org>
2021-01-06 10:14:49 -0800

    fix: fix auth for non-localhost DB's in node scripts

--- src/main/node/db/index.js
+++ src/main/node/db/index.js
@@ -1,6 +1,7 @@
 const pgp = require('pg-promise')({schema: 'cpic'});
 
-const cn = `postgres://cpic@${process.env.PGHOST}:5432/cpic`;
+const user = process.env.PGHOST === 'localhost' ? 'cpic' : `cpic:${process.env.PGPASS}`;
+const cn = `postgres://${user}@${process.env.PGHOST}:5432/cpic`;
 const db = pgp(cn);
 
 module.exports = db;
