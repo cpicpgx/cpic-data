a63799376e2d64454523bf431e5de4af9cc76b8c
Ryan Whaley <whaleyr@pharmgkb.org>
2022-02-14 11:20:08 -0800

    feat: add data for new statins guideline publication

---
+++ src/main/resources/db/migration/V53__statins_guideline.sql
@@ -0,0 +1,33 @@
+do $$
+    DECLARE
+        guidelineName TEXT := 'SLCO1B1, ABCG2, CYP2C9, and Statins';
+        guidelineUrl  TEXT := 'https://cpicpgx.org/guidelines/cpic-guideline-for-statins/';
+        statinGuidelineId INT  := -1;
+    BEGIN
+        -- update guideline info to supercede the previous simvastatin guideline
+        update guideline set name=guidelineName, genes='{SLCO1B1,ABCG2,CYP2C9}', url=guidelineUrl
+        where name='SLCO1B1 and Simvastatin' returning id into statinGuidelineId;
+
+        update publication set guidelineid=statinGuidelineId where pmid='35152405';
+
+        -- 1-gene pairs
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('SLCO1B1', 'A', (select drugid from drug where name='simvastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('SLCO1B1', 'A', (select drugid from drug where name='pravastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('SLCO1B1', 'A', (select drugid from drug where name='pitavastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('SLCO1B1', 'A', (select drugid from drug where name='atorvastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('SLCO1B1', 'A', (select drugid from drug where name='lovastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+
+        -- 2-gene pair for fluvastatin
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('SLCO1B1', 'A', (select drugid from drug where name='fluvastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('CYP2C9', 'A', (select drugid from drug where name='fluvastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+
+        -- 2-gene pair for rosuvastatin
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('SLCO1B1', 'A', (select drugid from drug where name='rosuvastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+        insert into pair as p (genesymbol, cpiclevel, drugid, guidelineid, usedforrecommendation, citations) values ('ABCG2', 'A', (select drugid from drug where name='rosuvastatin'), (select id from guideline where name=guidelineName), true, '{35152405}') on conflict (genesymbol, drugid) do update set cpiclevel=excluded.cpiclevel, guidelineid=excluded.guidelineid, usedforrecommendation=excluded.usedforrecommendation, citations=array_cat(p.citations, excluded.citations);
+
+        -- update drugs
+        update drug set guidelineid=(select id from guideline where name=guidelineName)
+        where name in (
+                       'simvastatin', 'rosuvastatin', 'pravastatin', 'pitavastatin', 'atorvastatin', 'fluvastatin', 'lovastatin'
+            );
+    END $$
