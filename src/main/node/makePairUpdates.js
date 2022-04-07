aba53d36fc583f1ad3faceccc4923532217b45b8
Ryan Whaley <whaleyr@pharmgkb.org>
2022-04-07 14:44:26 -0700

    feat: improve pair update process
    - add better logging
    - write changes to the change_log
    - do smart updates, only when the value changes
    - handle none/null values correctly

--- src/main/node/makePairUpdates.js
+++ src/main/node/makePairUpdates.js
@@ -1,5 +1,6 @@
 #!/bin/node
 const axios = require('axios');
+const log = require('./log');
 const db = require('./db');
 const _ = require('lodash');
 
@@ -28,7 +29,7 @@ const queryCurrentPairs = async () => {
 
 const requestUpdates = async (body) => {
     const pgkbApiUrl = `https://${pgkbApi}/v1/collaborator/cpic/pair`;
-    console.log(`Checking ${pgkbApiUrl}`);
+    log.debug(`Checking ${pgkbApiUrl}`);
     try {
         const response = await axios.post(
             pgkbApiUrl,
@@ -36,39 +37,82 @@ const requestUpdates = async (body) => {
         );
         return response.data;
     } catch (err) {
-        console.error('ERROR:', err.message);
+        log.error(err.message);
+    }
+};
+
+const hasCaChange = (change) => change.hasOwnProperty('pairid') && change.hasOwnProperty('pgkbcalevel');
+const hasTestingChange = (change) => change.hasOwnProperty('pairid') && change.hasOwnProperty('pgxtesting');
+const mapNoneToNull = (value) => {
+    if (!value || _.trim(value) === 'none') {
+        return null;
+    } else {
+        return _.trim(value);
+    }
+}
+const mapNullToNone = (value) => value || 'none';
+
+const writeChangeNote = async (message) => {
+    log.info(message);
+    return db.none(
+        `insert into change_log(date, type, note) values (current_date, 'PAIR', $(message))`,
+        {message},
+    );
+}
+
+const updateCaLevel = async ({pairid, pgkbcalevel: pgkbcalevelIn, name}) => {
+    const currentPair = await db.one(`select genesymbol, drugname, pgkbcalevel from pair_view where pairid=$(pairid)`, {pairid});
+    const pgkbcalevel = mapNoneToNull(pgkbcalevelIn);
+    if (currentPair.pgkbcalevel !== pgkbcalevel) {
+        await db.none(
+            `update pair
+             set pgkbcalevel=$(pgkbcalevel)
+             where pairid = $(pairid)`,
+            {pgkbcalevel, pairid},
+        );
+        return writeChangeNote(`[${currentPair.drugname}-${currentPair.genesymbol}] PharmGKB level changed from ${mapNullToNone(currentPair.pgkbcalevel)} to ${mapNullToNone(pgkbcalevel)}`);
+    } else {
+        log.debug(`Skipping ${name} since CA level already matches`);
+        return Promise.resolve();
+    }
+};
+
+const updateTesting = async ({pairid, pgxtesting: pgxtestingIn, name}) => {
+    const currentPair = await db.one(`select genesymbol, drugname, pgxtesting from pair_view where pairid=$(pairid)`, {pairid});
+    const pgxtesting = mapNoneToNull(pgxtestingIn);
+    if (currentPair.pgxtesting !== pgxtesting) {
+        await db.none(
+            `update pair
+             set pgxtesting=$(pgxtesting)
+             where pairid = $(pairid)`,
+            {pgxtesting, pairid},
+        );
+        return writeChangeNote(`[${currentPair.drugname}-${currentPair.genesymbol}] PGx testing level changed from ${mapNullToNone(currentPair.pgxtesting)} to ${mapNullToNone(pgxtesting)}`);
+    } else {
+        log.debug(`Skipping ${name} since PGx testing already matches`);
+        return Promise.resolve();
     }
 };
 
 const processChanges = async (changes) => {
-    await Promise.all(changes.filter(c => !!c.pairid && c.hasOwnProperty('pgkbcalevel')).map(({pgkbcalevel, pairid}) => {
-        console.log(`Updating ${pairid} to CA ${pgkbcalevel}`);
-        return db.none(`update pair
-                        set pgkbcalevel=$(pgkbcalevel)
-                        where pairid = $(pairid)`, {pgkbcalevel, pairid});
-    }));
-
-    await Promise.all(changes.filter(c => !!c.pairid && c.hasOwnProperty('pgxtesting')).map(({pgxtesting, pairid}) => {
-        console.log(`Updating ${pairid} to PGx ${pgxtesting}`);
-        return db.none(`update pair
-                        set pgxtesting=$(pgxtesting)
-                        where pairid = $(pairid)`, {pgxtesting, pairid});
-    }));
+    await Promise.all(changes.filter(hasCaChange).map(updateCaLevel));
+    await Promise.all(changes.filter(hasTestingChange).map(updateTesting));
 }
 
 try {
-    console.log(`Writing to DB: ${process.env.PGHOST || 'localhost'}`)
+    log.info('Checking for pair updates');
+    log.debug(`Writing to DB: ${process.env.PGHOST || 'localhost'}`);
     queryCurrentPairs()
         .then(async (data) => {
-            console.log(`${data?.length || 0} pairs in CPIC`)
+            log.debug(`${data?.length || 0} pairs in CPIC`)
             const changes = await requestUpdates(data);
             const updates = _.filter(changes?.data, (c) => !!c.pairid);
-            console.log(`${updates?.length || 0} changes found in PGKB`)
+            log.debug(`${updates?.length || 0} changes found in PGKB`)
             await processChanges(updates);
         })
-        .catch((err) => console.err('Error requesting updates', err))
+        .catch((err) => log.error('Error requesting updates', err))
         .finally(() => process.exit(0));
 } catch (err) {
-    console.error('Error updating pairs', err);
+    log.error('Error updating pairs', err);
     process.exit(1);
 }
