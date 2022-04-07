60899c2608539316691682082e4bb45b9594da36
Ryan Whaley <whaleyr@pharmgkb.org>
2022-04-06 20:14:24 -0700

    fix: fix error handling for makePairUpdates script

--- src/main/node/makePairUpdates.js
+++ src/main/node/makePairUpdates.js
@@ -19,49 +19,56 @@ const _ = require('lodash');
 const pgkbApi = process.env.PGKBAPI || 'localhost.pharmgkb.org:8543';
 
 const queryCurrentPairs = async () => {
-  return await db.many(`
-  select json_build_object('drugid', d.drugid, 'name', d.name, 'pharmgkbid', d.pharmgkbid) as drug,p.genesymbol,
-         p.pgkbcalevel, p.pgxtesting, p.pairid
-  from pair p join drug d on p.drugid=d.drugid where p.removed is false
-  `);
+    return await db.many(`
+        select json_build_object('drugid', d.drugid, 'name', d.name, 'pharmgkbid', d.pharmgkbid) as drug,p.genesymbol,
+               p.pgkbcalevel, p.pgxtesting, p.pairid
+        from pair p join drug d on p.drugid=d.drugid where p.removed is false
+    `);
 };
 
 const requestUpdates = async (body) => {
-  const response = await axios.post(
-    `https://${pgkbApi}/v1/collaborator/cpic/pair`,
-    body
-  );
-  return response.data;
+    const pgkbApiUrl = `https://${pgkbApi}/v1/collaborator/cpic/pair`;
+    console.log(`Checking ${pgkbApiUrl}`);
+    try {
+        const response = await axios.post(
+            pgkbApiUrl,
+            body
+        );
+        return response.data;
+    } catch (err) {
+        console.error('ERROR:', err.message);
+    }
 };
 
 const processChanges = async (changes) => {
-  await Promise.all(changes.filter(c => !!c.pairid && !!c.pgkbcalevel).map(({pgkbcalevel, pairid}) => {
-    console.log(`Updating ${pairid} to CA ${pgkbcalevel}`);
-    return db.none(`update pair
-             set pgkbcalevel=$(pgkbcalevel)
-             where pairid = $(pairid)`, {pgkbcalevel, pairid});
-  }));
+    await Promise.all(changes.filter(c => !!c.pairid && !!c.pgkbcalevel).map(({pgkbcalevel, pairid}) => {
+        console.log(`Updating ${pairid} to CA ${pgkbcalevel}`);
+        return db.none(`update pair
+                        set pgkbcalevel=$(pgkbcalevel)
+                        where pairid = $(pairid)`, {pgkbcalevel, pairid});
+    }));
 
-  await Promise.all(changes.filter(c => !!c.pairid && !!c.pgxtesting).map(({pgxtesting, pairid}) => {
-    console.log(`Updating ${pairid} to PGx ${pgxtesting}`);
-    return db.none(`update pair
-             set pgxtesting=$(pgxtesting)
-             where pairid = $(pairid)`, {pgxtesting, pairid});
-  }));
+    await Promise.all(changes.filter(c => !!c.pairid && !!c.pgxtesting).map(({pgxtesting, pairid}) => {
+        console.log(`Updating ${pairid} to PGx ${pgxtesting}`);
+        return db.none(`update pair
+                        set pgxtesting=$(pgxtesting)
+                        where pairid = $(pairid)`, {pgxtesting, pairid});
+    }));
 }
 
 try {
-   queryCurrentPairs()
-    .then(async (data) => {
-      console.log(`${data?.length || 0} pairs in CPIC`)
-      const changes = await requestUpdates(data);
-      const updates = _.filter(changes?.data, (c) => !!c.pairid);
-      console.log(`${updates?.length || 0} changes found in PGKB`)
-      await processChanges(updates);
-    })
-    .catch((err) => console.err('Error requesting updates', err))
-    .finally(() => process.exit(0));
+    console.log(`Writing to DB: ${process.env.PGHOST || 'localhost'}`)
+    queryCurrentPairs()
+        .then(async (data) => {
+            console.log(`${data?.length || 0} pairs in CPIC`)
+            const changes = await requestUpdates(data);
+            const updates = _.filter(changes?.data, (c) => !!c.pairid);
+            console.log(`${updates?.length || 0} changes found in PGKB`)
+            await processChanges(updates);
+        })
+        .catch((err) => console.err('Error requesting updates', err))
+        .finally(() => process.exit(0));
 } catch (err) {
-  console.error('Error updating pairs', err);
-  process.exit(1);
+    console.error('Error updating pairs', err);
+    process.exit(1);
 }
