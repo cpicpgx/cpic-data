ae55792172d1dc6ad17aca1dc4b8883f1ccfc56a
Ryan Whaley <whaleyr@pharmgkb.org>
2023-12-21 12:21:47 -0800

    fix: change default DB for node connections

--- src/main/node/db/index.js
+++ src/main/node/db/index.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')({schema: 'cpic'});
 
 const host = process.env.PGHOST || 'localhost';
-const dbname = process.env.PGDATABASE || 'cpic-staging';
+const dbname = process.env.PGDATABASE || 'cpic';
 const user = host === 'localhost' ? 'cpic' : `cpic:${process.env.PGPASS}`;
 
 const cn = `postgres://${user}@${host}:5432/${dbname}`;
